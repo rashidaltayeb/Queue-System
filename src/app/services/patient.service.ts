e881a724d4753e21f5482c5bde770d20ee331ba2
Rashid Altayeb Hassan <50522480+rashidaltayeb@users.noreply.github.com>
2022-08-16 19:49:58 +0200

    Update patient.service.ts

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -6,18 +6,31 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PatientService {
   URL = 'http://localhost:3000/patient/';
+  URL2 = 'http://localhost:3000/ticket/';
 
   constructor(private http: HttpClient) {}
   // get
   getPatient() {
     return this.http.get<any>(this.URL);
   }
+  /// get patient on waiting
+  getPatientOnWaiting() {
+    return this.http.get<any>(this.URL + '?service=3&_embed=ticket&_sort=ticketId');
+  }
   // post
   createPatient(data: any) {
     return this.http.post<any>(this.URL, data);
   }
   // put -> change service status
-  startService(data: any,id : number) {
-    return this.http.put<any>(this.URL+id, data)
+  startService(data: any, id: number) {
+    return this.http.put<any>(this.URL + id, data);
   }
+  /// add to queue
+  Queue(data: any) {
+    return this.http.post<any>(this.URL2, data);
+  }
+  // /// get sorted queue
+  // getSortedQueue() {
+  //   return this.http.get<any>(this.URL2);
+  // }
 }
