3a21ca0f8b295a287afd818b934bcf08a28e3a8d
Rashid Altayeb Hassan <50522480+rashidaltayeb@users.noreply.github.com>
2022-08-16 19:49:41 +0200

    Update waiting.component.ts

--- src/app/component/waiting/waiting.component.ts
+++ src/app/component/waiting/waiting.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormControl,FormGroup } from '@angular/forms';
+import { PatientService } from '../../services/patient.service';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-waiting',
   templateUrl: './waiting.component.html',
-  styleUrls: ['./waiting.component.css']
+  styleUrls: ['./waiting.component.css'],
 })
 export class WaitingComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'Name', 'Service', 'Ticket'];
+  dataSource!: MatTableDataSource<any>;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  nextQueue: any = [];
+  date = new FormControl(new Date());
+
+  constructor(
+    private PatientApi: PatientService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
+    // this.PatientWaiting();
+    this.PatientWaiting();
   }
+  ///// get all patient
+  PatientWaiting() {
+    this.PatientApi.getPatientOnWaiting().subscribe({
+      next: (res) => {
+        this.nextQueue = res[0];
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        alert('Error while get patient data');
+      },
+    });
+  }
+  ////
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
