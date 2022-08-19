29dfa435035de316da4035ea5ea4531ad76214c6
Rashid Altayeb Hassan <50522480+rashidaltayeb@users.noreply.github.com>
2022-08-19 21:33:16 +0200

    Update waiting.component.ts

--- src/app/component/waiting/waiting.component.ts
+++ src/app/component/waiting/waiting.component.ts
@@ -13,7 +13,7 @@ import {
 @Component({
   selector: 'app-waiting',
   templateUrl: './waiting.component.html',
-  styleUrls: ['./waiting.component.css'],
+  styleUrls: ['../../../assets/css/waiting.component.css'],
 })
 export class WaitingComponent implements OnInit {
   displayedColumns: string[] = ['Name', 'Service', 'Ticket', 'Finished'];
@@ -25,13 +25,13 @@ export class WaitingComponent implements OnInit {
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  patientUpdate: any
-  updateService : any
+  patientUpdate: any;
+  updateService: any;
   nextQueue: any = [];
   allTicket: any = [];
   patientOnProgress: any = [];
   doctorStatus: any;
-  doctorCheck: any
+  doctorCheck: any;
 
   constructor(
     private PatientApi: PatientService,
@@ -58,19 +58,22 @@ export class WaitingComponent implements OnInit {
   sentToDoctor(id: number) {
     ///
     this.patientUpdate.service = 4;
-    this.PatientApi.serviceProgress(this.updateService, this.patientUpdate.id).subscribe({
+    this.PatientApi.serviceProgress(
+      this.updateService,
+      this.patientUpdate.id
+    ).subscribe({
       next: (res) => {
-            this.doctorStatus.status = 1;
-            this.PatientApi.doctorStatus(1, this.doctorStatus).subscribe({
-              next: (res) => {
-                this.getDoctorStatus();
-                this.openSnackBar('patient now with the doctor', 'OK');
-              },
-              error: (err) => {
-                this.openSnackBar('can`t change doctor status', 'close');
-              },
-            });
-    this.PatientOn();
+        this.doctorStatus.status = 1;
+        this.PatientApi.doctorStatus(1, this.doctorStatus).subscribe({
+          next: (res) => {
+            this.getDoctorStatus();
+            this.openSnackBar('patient now with the doctor', 'OK');
+          },
+          error: (err) => {
+            this.openSnackBar('can`t change doctor status', 'close');
+          },
+        });
+        this.PatientOn();
       },
       error: (err) => {
         this.openSnackBar('Error while sent patient to doctor', 'close');
@@ -82,7 +85,7 @@ export class WaitingComponent implements OnInit {
     this.PatientApi.getDoctorStatus().subscribe({
       next: (res) => {
         this.doctorStatus = res[0];
-        this.doctorCheck = this.doctorStatus.status
+        this.doctorCheck = this.doctorStatus.status;
       },
       error: (err) => {
         this.openSnackBar('Error while get Doctor Status', 'close');
@@ -104,8 +107,8 @@ export class WaitingComponent implements OnInit {
     });
   }
   /////
-  removeTicket(row : any,id: number) {
-    this.updateService = row
+  removeTicket(row: any, id: number) {
+    this.updateService = row;
     this.PatientApi.removeTicket(id).subscribe({
       next: (res) => {
         this.PatientApi.getAllTicket().subscribe({
@@ -124,16 +127,16 @@ export class WaitingComponent implements OnInit {
   PatientOn() {
     this.PatientApi.getPatientOnService().subscribe({
       next: (res) => {
-        if (res.length != 1){
-          this.patientOnProgress = []
+        if (res.length != 1) {
+          this.patientOnProgress = [];
           return;
-        } 
+        }
         this.patientOnProgress = res[0];
       },
     });
   }
   /////
-  patientOut(){
+  patientOut() {
     this.patientOnProgress.service = 5;
     this.PatientApi.serviceProgress(
       this.patientOnProgress,
