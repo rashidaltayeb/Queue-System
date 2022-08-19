70a9b09362df8077d88ffb05148748a53bfb63ca
Rashid Altayeb Hassan <50522480+rashidaltayeb@users.noreply.github.com>
2022-08-19 23:13:07 +0200

    Update home.component.ts

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PatientService } from '../../services/patient.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['../../../assets/css/home.component.css']
+  styleUrls: ['../../../assets/css/home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  doctorCheck: any;
+  patientOnQueueCount: any;
+  patientCompleted: any;
+  patientOnHoldCount: any;
 
-  constructor() { }
+  constructor(private PatientApi: PatientService) {}
 
   ngOnInit(): void {
+    this.getDoctorStatus();
+    this.patientOnQueue();
+    this.patientOnCompleted();
+    this.patientOnHold();
   }
-
+  ////
+  getDoctorStatus() {
+    this.PatientApi.getDoctorStatus().subscribe({
+      next: (res) => {
+        this.doctorCheck = res[0].status;
+      },
+      error: (err) => {
+        return;
+      },
+    });
+  }
+  /////
+  patientOnQueue() {
+    this.PatientApi.patientOnQueue().subscribe({
+      next: (res) => {
+        this.patientOnQueueCount = res.length;
+      },
+      error: (err) => {
+        return;
+      },
+    });
+  }
+  ////
+  patientOnCompleted() {
+    this.PatientApi.completedPatient().subscribe({
+      next: (res) => {
+        this.patientCompleted = res.length;
+      },
+      error: (err) => {
+        return;
+      },
+    });
+  }
+  /////
+  patientOnHold() {
+    this.PatientApi.completedPatient().subscribe({
+      next: (res) => {
+        this.patientOnHoldCount = res.length;
+      },
+      error: (err) => {
+        return;
+      },
+    });
+  }
 }
